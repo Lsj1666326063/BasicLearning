193a193dc3631deab13f680245b5dff34c64949a
Lsj <1666326063@qq.com>
2021-11-09 18:46:18 +0800

    Signed-off-by: Lsj <1666326063@qq.com>

---
+++ BasicLearning/BasicLearning/DataStructure/SingleLinkedList.cs
@@ -0,0 +1,33 @@
+﻿namespace BasicLearning
+{
+    public class SingleLinkedList
+    {
+        private HeroNode head;
+
+        public SingleLinkedList()
+        {
+            head = new HeroNode(-1, "", "");
+        }
+
+        public void Add(HeroNode item)
+        {
+            
+        }
+        
+    }
+
+    public class HeroNode
+    {
+        public int Id;
+        public string Name;
+        public string NickName;
+        public HeroNode Next;
+
+        public HeroNode(int id, string name, string nickName)
+        {
+            Id = id;
+            Name = name;
+            NickName = nickName;
+        }
+    }
+}
